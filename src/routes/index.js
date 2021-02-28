9afd35b87f9979053965214733b28b028efcd8e9
WlBash <wlbash0909@gmail.com>
2021-02-28 22:38:05 +0800

    init program

---
+++ src/routes/index.js
@@ -0,0 +1,51 @@
+const router = require('koa-router')()
+
+router.get('/', async (ctx, next) => {
+    await ctx.render('index', {
+        title: 'Hello Koa 2!',
+        isMe: false,
+        blogList: [
+            {
+                id: 1,
+                title: 'aaa'
+            },
+            {
+                id: 2,
+                title: 'bbb'
+            },
+            {
+                id: 3,
+                title: 'vcc'
+            },
+        ]
+    })
+})
+
+router.get('/string', async (ctx, next) => {
+    ctx.body = 'koa2 string'
+})
+
+router.get('/json', async (ctx, next) => {
+    ctx.body = {
+        title: 'koa2 json'
+    }
+})
+
+router.get('/profile/:userName', async (ctx, next) => {
+    const { userName } = ctx.params
+    ctx.body = {
+        title: 'this is a profile',
+        userName
+    }
+})
+
+router.get('/loadMore/:userName/:pageIndex', async (ctx, next) => {
+    const { userName, pageIndex } = ctx.params
+    ctx.body = {
+        title: 'this is more',
+        userName,
+        pageIndex
+    }
+})
+
+module.exports = router
---
+++ src/routes/view/error.js
@@ -0,0 +1,11 @@
+const router = require('koa-router')()
+
+router.get('/error', async (ctx, next) => {
+    await ctx.render('error')
+})
+
+router.get('*', async (ctx, next) => {
+    await ctx.render('404')
+})
+
+module.exports = router
